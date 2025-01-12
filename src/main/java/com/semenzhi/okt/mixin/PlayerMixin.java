45fb6d4ea24ad4d10c53ae1b080a54fa7baa644b
Semeng <44737984+GMengZhi@users.noreply.github.com>
2025-01-13 00:16:37 +0800

    makes all falling blocks knockbackable by the player, depends on the knock back enchantment

--- src/main/java/com/semenzhi/okt/mixin/PlayerMixin.java
+++ src/main/java/com/semenzhi/okt/mixin/PlayerMixin.java
@@ -13,7 +13,6 @@ import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.event.entity.player.CriticalHitEvent;
 import net.neoforged.neoforge.event.entity.player.SweepAttackEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
