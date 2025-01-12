d1802f9630f9819fd61a28bbaa9a95c544454d15
Semeng <44737984+GMengZhi@users.noreply.github.com>
2025-01-12 13:40:23 +0800

    test

--- src/main/java/com/semenzhi/okt/mixin/ItemMixin.java
+++ src/main/java/com/semenzhi/okt/mixin/ItemMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,33 +27,15 @@ public class ItemMixin {
         DamageSource damageSource = new DamageSource(
                 level.registryAccess().lookupOrThrow(Registries.DAMAGE_TYPE).getOrThrow(APPLE_DAMAGE), null, player, null
         );
+        onUrKneeEntity$test();
         if(stack.getItem() == Items.APPLE && remainingUseDuration < 20 &&!level.isClientSide()) {
             player.hurtServer((ServerLevel) level,damageSource,Float.MAX_VALUE);
             player.displayClientMessage(Component.literal("You have poisoned by An Apple. You asshole"),false);
         }
     }
 
-
-    /*
-    @Inject(method = "finishUsingItem", at = @At("HEAD"))
-    public void finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir){
-
-        Player player = (Player) livingEntity;
-
-        DamageSource damageSource = new DamageSource(
-                level.registryAccess().lookupOrThrow(Registries.DAMAGE_TYPE).getOrThrow(APPLE_DAMAGE), null, player, null
-        );
-
-        if(player.getMainHandItem().getItem() == Items.APPLE) {
-            if(!level.isClientSide()) {
-
-                player.hurtServer((ServerLevel) level,damageSource,Float.MAX_VALUE);
-                //player.kill((ServerLevel) level);
-                //player.die(damageSource);
-                player.displayClientMessage(Component.literal("You have poisoned by An Apple. You asshole"),false);
-
-            }
-        }
+    @Unique
+    public void onUrKneeEntity$test(){
+        System.out.println("test");
     }
-     */
 }
