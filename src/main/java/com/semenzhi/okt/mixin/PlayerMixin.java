9f053d41ebfce43481d86e0462fa76305a093bfa
Semeng <meng2772887108@outlook.com>
2025-01-14 19:09:36 +0800

    use Mixin Extras instead to get local vars

--- src/main/java/com/semenzhi/okt/mixin/PlayerMixin.java
+++ src/main/java/com/semenzhi/okt/mixin/PlayerMixin.java
@@ -1,23 +1,18 @@
 package com.semenzhi.okt.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.semenzhi.okt.Connector;
 import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
-import net.neoforged.neoforge.event.entity.player.CriticalHitEvent;
-import net.neoforged.neoforge.event.entity.player.SweepAttackEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity{
@@ -26,13 +21,13 @@ public abstract class PlayerMixin extends LivingEntity{
         super(p_20966_, p_20967_);
     }
 
-    @Inject(method = "attack",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;push(DDD)V", shift = At.Shift.AFTER), locals = LocalCapture.PRINT)
-    public void attack(Entity target, CallbackInfo ci, float f, ItemStack itemstack, DamageSource damagesource, float f1, float f2, boolean flag3, boolean flag, boolean flag1, CriticalHitEvent critEvent, float f3, boolean flag2, boolean critBlocksSweep, SweepAttackEvent sweepEvent, float f6, Vec3 vec3, boolean flag4, float f4, LivingEntity livingentity1) {
+    @Inject(method = "attack",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;push(DDD)V", shift = At.Shift.AFTER))
+    public void attack(Entity target, CallbackInfo ci, @Local(ordinal = 5) float f4) {
 
         ((Connector) (target)).knockbackR(
-                (double)(f4 * 0.5F),
-                (double) Mth.sin(this.getYRot() * (float) (Math.PI / 180.0)),
-                (double)(-Mth.cos(this.getYRot() * (float) (Math.PI / 180.0)))
+                f4 * 0.5F,
+                Mth.sin(this.getYRot() * (float) (Math.PI / 180.0)),
+                -Mth.cos(this.getYRot() * (float) (Math.PI / 180.0))
         );
     }
 
