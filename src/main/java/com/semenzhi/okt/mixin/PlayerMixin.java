fd8d3f64aa7f1795dcd46e982caee1b469fa8a29
Semeng <meng2772887108@outlook.com>
2025-01-14 23:48:56 +0800

    now you can slap them

--- src/main/java/com/semenzhi/okt/mixin/PlayerMixin.java
+++ src/main/java/com/semenzhi/okt/mixin/PlayerMixin.java
@@ -1,7 +1,10 @@
 package com.semenzhi.okt.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import com.semenzhi.okt.Connector;
+import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -9,31 +12,51 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
-public abstract class PlayerMixin extends LivingEntity{
+public abstract class PlayerMixin extends LivingEntity {
 
     protected PlayerMixin(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
         super(p_20966_, p_20967_);
     }
 
-    @Inject(method = "attack",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;push(DDD)V", shift = At.Shift.AFTER))
-    public void attack(Entity target, CallbackInfo ci, @Local(ordinal = 5) float f4) {
-
-        ((Connector) (target)).knockbackR(
-                f4 * 0.5F,
-                Mth.sin(this.getYRot() * (float) (Math.PI / 180.0)),
-                -Mth.cos(this.getYRot() * (float) (Math.PI / 180.0))
-        );
-    }
+    @Shadow
+    public abstract void displayClientMessage(Component chatComponent, boolean actionBar);
 
     @ModifyVariable(method = "attack", at = @At("STORE"), ordinal = 5)
     public boolean flag4m(boolean flag) {
         return true;
     }
+
+    @ModifyExpressionValue(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getKnockback(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/damagesource/DamageSource;)F"))
+    public float attack$hands(float original) {
+        return (original == 0 ? original + 0.5F : original);
+
+    }
+
+    @Inject(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;push(DDD)V", shift = At.Shift.AFTER))
+    public void attack$knockback(Entity target, CallbackInfo ci,
+                                 @Local(index = 18) float knockBack,
+                                 @Local(index = 9) boolean isCritical,
+                                 @Local(index = 7) boolean isStrong,
+                                 @Local(index = 12) boolean isSweep) {
+
+        float knockbackMultiplier = isCritical ? 4.0F : (isStrong ? 2.0F : 0.5F);
+
+        ((Connector) (target)).knockbackR(
+                knockBack == 0F ? knockbackMultiplier * 0.5F : (knockBack * knockbackMultiplier * 0.5F),
+                Mth.sin(this.getYRot() * (float) (Math.PI / 180.0)),
+                -Mth.cos(this.getYRot() * (float) (Math.PI / 180.0))
+        );
+
+        displayClientMessage(Component.literal("kb" + (knockBack == 0F ? knockbackMultiplier * 0.5F : (knockBack * knockbackMultiplier * 0.5F))), true);
+
+    }
+
 
 }
